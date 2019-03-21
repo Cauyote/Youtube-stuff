bac21c815f039c6488ac311a11d1409d7c3233cb
Cauy Whaley <cauywhaley@area30.k12.in.us>
2019-03-21 10:46:59 -0400

    Youtube-stuff

---
+++ Coding stuff for hex color changer/app.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+        long = position,coords.longitude;
+        lat = position.coords.latitude;
+
+        const api ='https://api.darksky.net/forecast/fb1e0f5ca6400acae3cc5b740dbc43a5/${lat},${long}';
+    })
+}else{
+    h1.textContent = "Hey chief, it ain't working."
+}
+})
